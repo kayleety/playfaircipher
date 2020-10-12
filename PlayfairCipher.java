f9636a4dbaa97aacd33e79736e41c508b93b7264
Kaylee <kayleeyin2@gmail.com>
2020-10-11 21:20:19 -0400

    position keytext into array

--- PlayfairCipher.java
+++ PlayfairCipher.java
@@ -1,8 +1,28 @@
-// Encode
-// Test
+/***************************
+* PLAYFAIR CIPHER
+*
+* Kaylee Yin
+* Cybersecurity, Period 5
+***************************/
 
-// Second test
+public class PlayfairCipher{
 
-public static void main(String [] args){
+  // ENCODE
+  //public static encode
 
+  public static void main(String [] args){
+    String text = args[0];
+    String keytext = args[1];
+
+    // position the keytext into an array
+    String[][] key = new String[5][5];
+    int index = 0;
+    for (int i = 0; i < 5; i++){
+      for (int j = 0; j < 5; j++){
+        key[i][j] = "" + keytext.charAt(index);
+        index++;
+      }
+    }
+
+  }
 }
